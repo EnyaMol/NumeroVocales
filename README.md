a62fb8fba974ceaf553bcac5756cfd7af18be9f2
Ayinee <onaienya@gmail.com>
2018-10-10 16:13:53 +0200

    Actualitazacio del programa i explicacio de funcionament

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NumeroVocales
+
+#En el programa comprova les lletres de tota la cadena i suma les vocals totes juntes i despres les que son O.
--- numvocals.js
+++ numvocals.js
@@ -2,13 +2,17 @@ var cadena = prompt("Introdueix una frase: ");
 
 function comptarvocals(cadena){
   var contador =0;
+  var contadordeO=0;
   for (var i = 0; i < cadena.length; i++) {
     if (cadena.charAt(i)=='a' || cadena.charAt(i)=='e' || cadena.charAt(i)=='i'
     || cadena.charAt(i)=='o' || cadena.charAt(i)=='u' ) {
       contador++;
+      if (cadena.charAt(i)=='o') {
+        contadordeO++;
+      }
     }
   }
-  alert("La frase té "+contador+" vocals.");
+  alert("La frase té "+contador+" vocals. I conté "+contadordeO+ " vocals o.");
 }
 
 comptarvocals(cadena);
